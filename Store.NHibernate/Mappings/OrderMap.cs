59dba3bc3947f6bec8bd4b4ad25efcdea526ef84
Frylle Bancaso <akosifrylle@gmail.com>
2015-10-21 00:34:05 +0800

    Added Order mapping

---
+++ Store.NHibernate/Mappings/OrderMap.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Mapping;
+using Store.NHibernate.Repo;
+
+namespace Store.NHibernate.Mappings
+{
+    public class OrderMap : ClassMap<OrderRepo>
+    {
+        public OrderMap()
+        {
+            Table("[Order]");
+            Id(x => x.Id)
+                .GeneratedBy.Identity();
+            Map(x => x.OrderDate)
+                .CustomType("datetime").Not.Nullable();
+            References(x => x.User)
+                .Column("UserId")
+                .Cascade.All();
+        }
+    }
+}
